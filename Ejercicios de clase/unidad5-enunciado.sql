beb68c4e17e97830d9da2aacf52033579a03a689
eafernandezm01 <eafernandezm01@educarex.es>
2024-04-27 19:25:20 +0200

    enunciado

---
+++ Ejercicios de clase/unidad5-enunciado.sql
@@ -0,0 +1,130 @@
+-- Contar el número total de empleados.
+
+
+-- Encontrar la edad máxima entre los empleados.
+
+
+-- Encontrar la edad mínima entre los empleados.
+
+
+-- Calcular el salario promedio de los empleados. Redondeado a 2 decimales.
+
+
+-- Contar el número de empleados por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Encontrar el salario máximo por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Encontrar el salario mínimo por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Calcular el salario promedio por departamento. Si el departamento es nulo, poner 'Sin Dpto'. Redondeado a 2 decimales
+
+
+-- Calcular el total de comisiones por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Encontrar el salario máximo de los empleados por departamento, siempre que el departamento no sea nulo. Ordenar de mayor a menor salario.
+
+
+-- Modifica la anterior consulta para sacar solo los 3 departamentos que mas ganan.
+
+
+-- Encontrar el salario máximo de los empleados por departamento, siempre que el departamento no sea nulo. Solo debe obtener aquellos máximos mayores de 50000. Ordenar de mayor a menor salario.
+
+
+-- Consulta de empleados obteniendo su nombre_completo y salario. Siempre que el salario sea mayor de 25000 ordena por nombre_completo.
+
+
+-- Consulta de empleados obteniendo su nombre_completo, edad, salario, siempre que estos tenga edad entre 20-30 años y su salario oscile entre 25000-32000 ordena de manera descendente por salario. Con between.
+
+
+-- Consulta de empleados obteniendo su nombre_completo, edad, salario, siempre que estos tenga edad entre 20-30 años y su salario oscile entre 25000-32000 ordena de manera descendente por salario. Sin between.
+
+
+-- Consulta todos los empleados (tanto internos como externos) mostrando nombre + 1 apellido + 2 apellido (en una columna llamada nombre completo), fecha_contratacion o subcontratacion, y tipo ('Interno o Subcontratado'). Quitando repetidos.
+-- Ordena por nombre_completo
+
+
+
+
+-- Consulta todos los empleados (tanto internos como externos) mostrando nombre + 1 apellido + 2 apellido (en una columna llamada nombre completo), fecha_contratacion o subcontratacion, y tipo ('Interno o Subcontratado'). Con repetidos.
+-- Ordena por nombre_completo
+
+
+
+
+-- Consulta los años que lleva cada empleado externo trabajando, mostrar el id, nombre, apellido1, fecha_subcontratacion y num años. Trunca a la baja el numero de años.
+
+
+
+
+-- Partiendo de la consulta anterior obten el que mas tiempo lleva trabajando. Solo debe devolver un registro
+
+
+
+
+-- Consulta cuantos empleados por departamento existen que tenga un salario de más de 40000 y que cobren comision.  Debe aparecer el id_departamento y el total por departamento
+
+
+
+
+-- Consulta los datos de la venta realizada por el empleado 1 ordenados por fecha mas reciente
+
+
+
+
+-- Consulta los datos la ultima venta realizada por el empleado 1
+
+
+
+-- Consulta los datos de la ventas realizadas en abril
+
+
+
+-- Consulta los datos la maxima venta realizada en abril
+
+
+
+-- Cuenta el numero de ventas realizadas por el empleado 1
+
+
+
+-- Obten los empleados externos que nacieron 1981 
+
+
+
+-- Cuenta los empleados externos que nacieron en 1981
+
+
+
+-- Obten los empleados externos que nacieron en noviembre
+
+
+
+-- Obten maximo año de nacimiento de los empleados
+
+
+
+-- Obten los nombres y apellidos de los empleados (1 columna), la edad y 
+-- según el tramo de edad, es lo que debe valer plan:
+-- > 55 = Plan mayores de 55
+-- > 35 = Sin ventajas
+-- Resto = Plan joven 
+
+
+
+
+
+
+
+-- Haz lo mismo pero con los empleados externos.
+
+
+
+
+
+
+
+-- Haz lo mismo con los empleados interno y externo en la misma consulta, diferenciando cuales son internos o externos mediante una nueva columna.
