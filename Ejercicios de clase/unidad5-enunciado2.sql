d2bb6d40298d47bc03aa810f67c473561175fd0c
eafernandezm01 <eafernandezm01@educarex.es>
2024-04-28 13:02:24 +0200

    mas enunciado y mas datos

---
+++ Ejercicios de clase/unidad5-enunciado2.sql
@@ -0,0 +1,22 @@
+-- Total de empleados por departamento con más de 3 empleados
+
+
+-- Total de ventas realizadas por cada empleado con más de 5 ventas
+
+
+-- Cantidad de empleados externos por departamento
+
+
+-- Total de ventas realizadas por cada empleado en el departamento de Ventas
+
+
+-- Total de importe de ventas realizadas por cada empleado mientras que dicho total sea superior a 1000
+
+
+-- Número de departamentos con más de 100 empleados
+
+
+-- Cantidad de empleados que ganan más de 40000 por departamento
+
+
+-- Total de ventas realizadas en el año 2023 por cada empleado
