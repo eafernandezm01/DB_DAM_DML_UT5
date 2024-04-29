54b7d613996974cdb4bd42457d2ab6ee00fac073
eafernandezm01 <eafernandezm01@educarex.es>
2024-04-29 21:33:43 +0200

    subconsulta

---
+++ Ejercicios de clase/unidad5-enunciado3.sql
@@ -0,0 +1,45 @@
+/** SOLO REALIZARLO CON SUBCONSULTAS **/
+
+-- Encuentra todos los empleados que trabajan en el departamento de Marketing.
+
+-- Encuentra el nombre del departamento en el que trabaja Carlos Pérez González.
+
+-- Encuentra el id, nombre y apellido del jefe de Ana Fernández.
+
+-- Encuentra los empleados cuyo salario sea mayor que el de Laura Hernández Fernández. Ordena por salario de manera descendente.
+
+-- Encuentra los empleados que tienen una edad mayor que el promedio de edad de todos los empleados. Ordena por edad de manera ascendente
+
+-- Encuentra los empleados que tienen una edad menor que la edad del jefe de su departamento.
+
+-- Encuentra los departamentos cuyos empleado del departamento tenga una comisión mayor que la comisión promedio de todos los empleados.
+
+-- Encuentra el nombre de la empresa que subcontrató a Iván García.
+
+-- Encuentra el nombre y apellido (en una columna), fecha de subcontración en format DD/MM/YYYY de todos los empleados que fueron subcontratados después de que Eva García López fue subcontratada. Ordena por el nombre completo y por fecha
+
+-- Encuentra los empleados que realizaron ventas
+
+-- Encuentra los empleados que realizaron ventas en el año 2021
+
+-- Encuentra los empleados que NO realizaron ventas
+
+-- Encuentra el nombre y apellido en una columna de los empleados externos que fueron subcontratados por la empresa con CIF 'J8765432F'.
+
+-- Encuentra los empleados que trabajan en el mismo departamento que Iván Garcia (y no tiene segundo apellido) y que tienen un salario menor que el suyo.
+
+-- Encuentra los empleados que trabajan en el mismo departamento que Iván Garcia (y no tiene segundo apellido) y que tienen un salario menor que el suyo. Sin correlacionar
+
+-- Encuentra el nombre y apellidos del jefe del jefe de Elena Pérez Gómez.
+
+-- Encuentra todos los empleados cuyo departamento está en los departamentos de 'Ventas', 'Marketing' y 'IT'.
+
+-- Encuentra los departamentos cuyos nombres no están en la lista 'Ventas', 'Marketing' y 'IT'.
+
+-- Encuentra los empleados que no han realizado ventas.
+
+-- Encuentra los empleados externos que fueron subcontratados por las empresas 'Empresa A', 'Empresa B' y 'Empresa C'.
+
+-- Empleados cuyo salario es mayor que al menos uno de los empleados del departamento de Ventas.
+
+-- Empleados cuyo salario es mayor que todos de los empleados del departamento de Ventas.
