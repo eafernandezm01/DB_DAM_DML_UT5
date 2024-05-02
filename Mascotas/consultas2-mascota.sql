e9da2b888725a7db0f1b09e49bc6a93167eeff0f
eafernandezm01 <eafernandezm01@educarex.es>
2024-05-02 22:55:54 +0200

    .

---
+++ Mascotas/consultas2-mascota.sql
@@ -0,0 +1,48 @@
+
+
+-- Encuentra el nombre, tipo y edad de todos los animales adoptados. Ordena por tipo asc
+
+
+-- Muestra el nombre y la dirección de las personas que adoptaron perros.
+
+
+-- Muestra el nombre y la dirección de las personas que adoptaron perros, gatos y conejos.
+
+
+-- Encuentra el nombre, el tipo de todos los animales además del nombre completo del dueño y su edad. Solo aquellos adoptados por personas mayores de 30 años. Ordena por nombre completo.
+
+
+-- Muestra el nombre y la edad de todas las personas que adoptaron animales después del año 2023.
+
+
+-- Encuentra el nombre y el tipo de todos los animales adoptados cuyo apellido1 o apellido2 de su dueño contiene la letra "a".
+
+
+-- Muestra el nombre y el tipo de todos los animales adoptados por personas cuyo código postal empieza por "28".
+
+
+-- Encuentra los nombres de las personas que comparten el mismo número de teléfono.
+
+
+-- Encuentra los nombres de las personas que viven en la misma dirección.
+
+
+-- Muestra los nombres de las personas que comparten el mismo código postal.
+
+
+-- Muestra el nombre de todas las personas junto con los nombres de los animales que han adoptado, si es que han adoptado alguno.
+
+
+-- Muestra el nombre de todos los animales junto con los nombres de las personas que los adoptaron, si es que fueron adoptados.
+
+
+-- Muestra el nombre de todas las personas que han adoptado animales, su correo, junto con el nombre del animal. Filtra por aquellos que tienen email.
+
+
+-- Combina todas las personas con todos los animales.
+
+
+-- Combina todas las personas que tienen menos de 30 años con todos los animales que son de tipo "Gato". Muestra el nombre de la personal, el del animal
+
+
+-- Combina todas las personas cuyo nombre empieza por "A" con todos los animales que son de tipo "Perro".
