5393d0e8d396ca4c4667738e77ea0dd31ceabfe4
eafernandezm01 <eafernandezm01@educarex.es>
2024-05-10 08:41:28 +0200

    1

---
+++ Mascotas/consultas4-mascotas.sql
@@ -0,0 +1,198 @@
+
+
+-- Obtener el nombre y apellido de las personas que tienen una dirección de correo electrónico especificada.
+
+
+-- Mostrar el nombre y la edad de los animales que pesan más de 10 kilogramos.
+
+
+
+
+-- Mostrar los dni de las personas que han adoptado al menos un animal.
+
+
+
+
+-- Contar el número total de personas que han adoptado al menos un animal.
+
+
+
+
+-- Calcular el promedio de edad de los animales adoptados por personas mayores de 30 años.
+
+
+
+-- Mostrar los dni de las personas que ha adoptado mas de un animal, debe aparecer el NIF y el total de animales.
+
+
+    
+-- Mostrar el nombre y apellidos de las personas en una unica columna que han adoptado más de un animal. Con subselect en el where
+
+
+
+
+-- Mostrar el nombre y apellidos de las personas en una unica columna que han adoptado más de un animal, además muesta el total de los animales. Con subselect en el from-join
+
+
+
+
+-- Obtener el nombre y el tipo de animal de los animales adoptados por personas que viven en una 'Calle' y lo que sea.
+
+
+
+
+-- Contar el número de animales adoptados por personas con una edad mayor a 40 años.
+
+
+
+
+-- Calcular el peso total de todos los animales adoptados.
+
+
+
+
+-- Calcular el peso total de todos los animales adoptados que estén vivos
+
+
+
+
+--  Mostrar el nombre, apellido1 y apellido2 (columnas distintas). de las personas que han adoptado al menos un animal pequeño.
+
+
+
+
+-- Si la anterior la has hecho con subselect esta vez hazla con join o viceversa.
+
+
+
+
+-- Obtener el nombre, el tipo de animal, nif del propietario de los animales adoptados en 2024.
+
+
+
+
+-- Contar el número de personas que han adoptado pero sus animales no son mayores o igual de 2  años.
+
+
+
+
+-- Calcular el peso promedio de los animales adoptados por cada sexo. Mostrar el sexo y el promedio.
+
+
+
+
+-- Mostrar el nombre, apellido1 y 2 de las personas que han adoptado animales de más de una raza.
+
+
+
+
+-- Obtener el nombre y la edad de los animales adoptados por personas cuyo apellido empieza con "G".
+
+
+
+
+-- Contar el número total de animales adoptados en el año 2023.
+
+
+
+
+-- Cuenta el número de animales adoptados por tipo en el 2023, debe aparecer el nombre del tipo de animal también,
+
+
+
+
+-- Cuenta el defunciones por año de los animales, debe aparecer el año y el total
+
+
+
+
+-- Calcular el promedio de peso de los animales adoptados por personas menores de 25 años.
+
+
+
+
+-- Mostrar el nombre de las personas que han adoptado al menos un animal de color blanco.
+
+
+
+
+-- Mostrar el nombre de la persona, nombre del animal, tamaño y color que han adoptado al menos un animal de color blanco. 
+
+
+
+    
+-- Calcula el peso medio de todos los animales que han sido adoptados, redondea a 2 decimales
+
+
+
+-- Calcula el peso medio de todos los animales que han sido adoptados según su sexo, redondea a 2 decimales. 
+-- Este tipo de enunciados además del dato calculado por la funcion de agregación, debemos mostrar el criterio por el que nos han pedido.
+
+
+
+
+
+
+-- Calcula el maximo peso de los animales que han sido adaptado según su chip
+-- Este tipo de enunciados además del dato calculado por la funcion de agregación, debemos mostrar el criterio por el que nos han pedido.
+
+
+
+
+
+
+-- Cuenta los animales que han sido adoptados segun su chip
+-- Este tipo de enunciados además del dato calculado por la funcion de agregación, debemos mostrar el criterio por el que nos han pedido.
+
+
+
+
+
+
+-- Muestra toda informacion de los animales que tienen chip y su peso supera la media de los adoptados con chip
+
+
+-- Prueba poniendo la subconsulta en un join, para poder mostrar además de la informacion de los animales, el peso medio. Ordena por peso.
+
+
+-- Obtener el nombre y el tipo de animal de los animales adoptados por personas que viven en Barcelona.
+
+
+-- Calcular el peso total de todos los animales adoptados por personas mayores de 30 años.
+
+
+-- Mostrar el nombre de las personas que han adoptado animales de al menos 5 años de edad.
+
+
+-- Obtener el nombre de los tipos de animales cuya edad promedio de adopción es menor a 2 años.
+
+
+-- Consulta las personas que han adorptado animales de mas de un color dominante. Obten todos los datos de la persona
+
+
+-- Contar el número de personas que han adoptado animales de más de un color dominante.
+
+
+-- Mostrar el nombre de las personas que han adoptado al menos un animal de más de 20 kilogramos.
+
+
+-- Obtener el nombre, la edad, y el nombre y edad de las personas que lo han adoptado.
+
+
+-- Contar el número total de animales adoptados por personas con un correo electrónico especificado.
+
+
+-- Mostrar la informacion de las personas que han adoptado animales de más de una especie: 'Pez betta', 'Canario'
+
+
+-- Obtener el nombre de los tipos de animales que han sido adoptados por personas menores de 30 años.
+
+
+-- Calcular el promedio de altura de los animales adoptados por cada tipo de animal. Debe aparecer el nombre y el promedio redondeado a 2 decimale
+
+
+-- Mostrar el nombre de las personas que han adoptado al menos un animal con chip.
+
+
+-- Obtener el nombre y la edad de los animales adoptados por personas que tienen entre 25 y 35 años.
+
